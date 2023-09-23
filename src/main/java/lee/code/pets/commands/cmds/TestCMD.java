223be8d62dbe78dd2269bc5298bea52f46bbf33f
Lee <leecook808@gmail.com>
2023-09-23 00:44:57 -0700

    WanderingTraderPet

--- src/main/java/lee/code/pets/commands/cmds/TestCMD.java
+++ src/main/java/lee/code/pets/commands/cmds/TestCMD.java
@@ -48,7 +48,7 @@ public class TestCMD extends SubCommand {
 
   @Override
   public void perform(Player player, String[] args) {
-    pets.getPetManager().spawn(player, EntityType.TROPICAL_FISH, new String[]{"&d&lGIGA CHAD", "FLOPPER", "RED", "BLUE"});
+    pets.getPetManager().spawn(player, EntityType.WANDERING_TRADER, new String[]{"&d&lGIGA CHAD"});
   }
 
   @Override
--- src/main/java/lee/code/pets/pets/PetManager.java
+++ src/main/java/lee/code/pets/pets/PetManager.java
@@ -64,6 +64,7 @@ public class PetManager  {
       case ZOMBIE_HORSE -> spawn(player.getWorld(), new ZombieHorsePet(player, Boolean.parseBoolean(data[0]), data[1]));
       case TADPOLE -> spawn(player.getWorld(), new TadpolePet(player, data[0]));
       case TROPICAL_FISH -> spawn(player.getWorld(), new TropicalFishPet(player, data[0], data[1], data[2], data[3]));
+      case WANDERING_TRADER -> spawn(player.getWorld(), new WanderingTraderPet(player, data[0]));
     }
   }
 
