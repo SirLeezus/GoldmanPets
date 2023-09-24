2a3b9d9c3f60260d06504ad297288391d1bb752f
Lee <leecook808@gmail.com>
2023-09-24 01:17:14 -0700

    ILLUSIONER

--- src/main/java/lee/code/pets/pets/PetManager.java
+++ src/main/java/lee/code/pets/pets/PetManager.java
@@ -89,6 +89,7 @@ public class PetManager  {
       case WITHER_SKELETON -> spawn(player.getWorld(), new WitherSkeletonPet(player, data[0]));
       case ZOMBIE_VILLAGER -> spawn(player.getWorld(), new ZombieVillagerPet(player, Boolean.parseBoolean(data[0]), data[1], data[2], data[3], data[4]));
       case EVOKER -> spawn(player.getWorld(), new EvokerPet(player, data[0]));
+      case ILLUSIONER -> spawn(player.getWorld(), new IllusionerPet(player, data[0]));
     }
   }
 
