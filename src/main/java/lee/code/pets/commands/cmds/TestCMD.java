b4cfeb8132e312056259337145263d10fc6ec571
Lee <leecook808@gmail.com>
2023-09-24 18:28:04 -0700

    snowman fix

--- src/main/java/lee/code/pets/commands/cmds/TestCMD.java
+++ src/main/java/lee/code/pets/commands/cmds/TestCMD.java
@@ -48,7 +48,7 @@ public class TestCMD extends SubCommand {
 
   @Override
   public void perform(Player player, String[] args) {
-    pets.getPetManager().spawn(player, EntityType.SNIFFER, new String[]{"false", "&d&lGIGA CHAD"});
+    pets.getPetManager().spawn(player, EntityType.SNOWMAN, new String[]{"&d&lGIGA CHAD", "true"});
   }
 
   @Override
--- src/main/java/lee/code/pets/pets/pet/animal/SnowGolemPet.java
+++ src/main/java/lee/code/pets/pets/pet/animal/SnowGolemPet.java
@@ -5,16 +5,20 @@ import lee.code.pets.pets.goals.FollowOwnerGoal;
 import lee.code.pets.utils.CoreUtil;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.syncher.EntityDataAccessor;
+import net.minecraft.network.syncher.EntityDataSerializers;
+import net.minecraft.network.syncher.SynchedEntityData;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.animal.SnowGolem;
+import net.minecraft.world.entity.Mob;
 import org.bukkit.craftbukkit.v1_20_R1.CraftWorld;
 import org.bukkit.craftbukkit.v1_20_R1.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.event.entity.EntityTargetEvent;
 
-public class SnowGolemPet extends SnowGolem {
-
-  //TODO CANCEL SNOW ON GROUND WHEN MOVING
+public class SnowGolemPet extends Mob {
+  private static final EntityDataAccessor<Byte> DATA_PUMPKIN_ID = SynchedEntityData.defineId(SnowGolemPet.class, EntityDataSerializers.BYTE);
 
   public SnowGolemPet(Player player, String name, boolean pumpkin) {
     super(EntityType.SNOW_GOLEM, ((CraftWorld) player.getLocation().getWorld()).getHandle());
@@ -36,6 +40,44 @@ public class SnowGolemPet extends SnowGolem {
   @Override
   protected void registerGoals() {
     goalSelector.addGoal(0, new FollowOwnerGoal(this, 2));
+  }
+
+  @Override
+  protected SoundEvent getAmbientSound() {
+    return SoundEvents.SNOW_GOLEM_AMBIENT;
+  }
+
+  @Override
+  protected void defineSynchedData() {
+    super.defineSynchedData();
+    entityData.define(DATA_PUMPKIN_ID, (byte) 16);
+  }
+
+  public boolean hasPumpkin() {
+    return (entityData.get(DATA_PUMPKIN_ID) & 16) != 0;
+  }
+
+  public void setPumpkin(boolean hasPumpkin) {
+    final byte b0 = entityData.get(DATA_PUMPKIN_ID);
+    if (hasPumpkin) {
+      this.entityData.set(DATA_PUMPKIN_ID, (byte) (b0 | 16));
+    } else {
+      this.entityData.set(DATA_PUMPKIN_ID, (byte) (b0 & -17));
+    }
+  }
+
+  @Override
+  public void addAdditionalSaveData(CompoundTag nbt) {
+    super.addAdditionalSaveData(nbt);
+    nbt.putBoolean("Pumpkin", hasPumpkin());
+  }
+
+  @Override
+  public void readAdditionalSaveData(CompoundTag nbt) {
+    super.readAdditionalSaveData(nbt);
+    if (nbt.contains("Pumpkin")) {
+      setPumpkin(nbt.getBoolean("Pumpkin"));
+    }
   }
 
   @Override
