e21475a7787fa2a1ab4f754d9ccef5dce8b2502c
Lee <leecook808@gmail.com>
2023-09-24 18:33:36 -0700

    WanderingTrader fix

--- src/main/java/lee/code/pets/commands/cmds/TestCMD.java
+++ src/main/java/lee/code/pets/commands/cmds/TestCMD.java
@@ -48,7 +48,7 @@ public class TestCMD extends SubCommand {
 
   @Override
   public void perform(Player player, String[] args) {
-    pets.getPetManager().spawn(player, EntityType.SNOWMAN, new String[]{"&d&lGIGA CHAD", "true"});
+    pets.getPetManager().spawn(player, EntityType.WANDERING_TRADER, new String[]{"&d&lGIGA CHAD"});
   }
 
   @Override
