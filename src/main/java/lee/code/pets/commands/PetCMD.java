d0234eb07e54a544f42d1f17e5970423481283bd
Lee <leecook808@gmail.com>
2023-10-16 10:46:02 -0700

    HelpCMD update

--- src/main/java/lee/code/pets/commands/PetCMD.java
+++ src/main/java/lee/code/pets/commands/PetCMD.java
@@ -3,11 +3,15 @@ package lee.code.pets.commands;
 import lee.code.pets.Pets;
 import lee.code.pets.lang.Lang;
 import lee.code.pets.menus.menu.PetMenu;
+import net.kyori.adventure.text.Component;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PetCMD implements CommandExecutor {
   private final Pets pets;
@@ -20,6 +24,20 @@ public class PetCMD implements CommandExecutor {
   public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
     if (!(sender instanceof Player player)) {
       sender.sendMessage(Lang.PREFIX.getComponent(null).append(Lang.ERROR_NOT_CONSOLE_COMMAND.getComponent(null)));
+      return true;
+    }
+    if (args.length > 0 && args[0].equalsIgnoreCase("help")) {
+      final List<Component> lines = new ArrayList<>();
+      lines.add(Lang.COMMAND_HELP_TITLE.getComponent(null));
+      lines.add(Component.text(""));
+      lines.add(Lang.COMMAND_HELP_LINE_1.getComponent(null));
+      lines.add(Lang.COMMAND_HELP_LINE_2.getComponent(null));
+      lines.add(Lang.COMMAND_HELP_LINE_3.getComponent(null));
+      lines.add(Lang.COMMAND_HELP_LINE_4.getComponent(null));
+      lines.add(Lang.COMMAND_HELP_LINE_5.getComponent(null));
+      lines.add(Component.text(""));
+      lines.add(Lang.COMMAND_HELP_FOOTER.getComponent(null));
+      for (Component line : lines) sender.sendMessage(line);
       return true;
     }
     pets.getMenuManager().openMenu(new PetMenu(pets), player);
--- src/main/java/lee/code/pets/lang/Lang.java
+++ src/main/java/lee/code/pets/lang/Lang.java
@@ -8,6 +8,13 @@ import net.kyori.adventure.text.Component;
 @AllArgsConstructor
 public enum Lang {
   PREFIX("&5&lPets &6➔ "),
+  COMMAND_HELP_TITLE("&a------------------ &7[ &5&lPet Help &7] &a------------------"),
+  COMMAND_HELP_LINE_1("&eYou can capture pets by purchasing a &5Pet Capture Lead"),
+  COMMAND_HELP_LINE_2("&efrom &3/shop&e. By default you can capture 3 entities,"),
+  COMMAND_HELP_LINE_3("&eif you want more you can subscribe to a premium rank."),
+  COMMAND_HELP_LINE_4("&eAll entities captured can be found in your &3/pet &emenu."),
+  COMMAND_HELP_LINE_5("&eAll entities can be captured besides the ender dragon."),
+  COMMAND_HELP_FOOTER("&a-------------------------------------------------"),
   LEAD_HEALTH("&6&lHealth&7: {0}"),
   MENU_PETS_TITLE("&5&lPets"),
   MENU_PET_OPTIONS_TITLE("&5&lPet Options"),
@@ -18,7 +25,7 @@ public enum Lang {
   MENU_RENAME_SUCCESSFUL("&aYou successfully renamed your pet to &f{0}&a!"),
   MENU_RENAME_MESSAGE("&aPlease type your new pet's name in chat&7:"),
   MENU_PET_ITEM_NAME("&e&lPet Name&7:"),
-  MENU_PET_ITEM_LORE("&f{0}\n \n&e&lActions&7:\n&e» &7Right-Click &eEdit Pet\n&e» &7Left-Click &eSpawn Pet\n&e» &7Shift-Click &eDelete Pet"),
+  MENU_PET_ITEM_LORE("&f{0}\n \n&e&lActions&7:\n&e» &7Right-Click &eEdit Pet\n&e» &7Left-Click &eSpawn Pet\n&e» &7Sneak-Click &eDelete Pet"),
   CAPTURE_SUCCESSFUL("&aYou successfully captured a &3{0}&a!"),
   ERROR_RENAME_COMMAND("&cYou can't use commands until you type in a new pet name."),
   ERROR_NO_PERMISSION("&cYou do not have permission for this."),
